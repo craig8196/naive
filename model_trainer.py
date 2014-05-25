b32adc324f186a41b33ae0fe978e2de03d1867ab
willpalm <will5ryanpalmer@gmail.com>
2014-05-24 19:01:02 -0600

    added code to read the number of files

--- model_trainer.py
+++ model_trainer.py
@@ -1,10 +1,18 @@
+import os
+import os.path
+import VocabTrie from vocab_trie
+
 class ModelTrainer(object):
 
 	MULTIVARIATE = 1;
 	MULTINOMIAL = 2;
 	SMOOTHED = 3;
 
-	def __init__(self, type):
+	def __init__(self, type, dir):
+		self.stop_words = self.fill_stop_list();
+		self.train_dir = dir
+		self.group_map = self.init_group_map();
+
 		if type == ModelTrainer.MULTIVARIATE:
 			self.train_multivariate()
 		elif type == ModelTrainer.MULTINOMIAL:
@@ -12,11 +20,38 @@ class ModelTrainer(object):
 		elif type == ModelTrainer.SMOOTHED:
 			self.train_smoothed()
 
+	def fill_stop_list(self):
+		stop_list = []
+		f = open('stoplist.txt', 'r')
+		for line in f:
+			stop_list.append(line.rstrip())
+		return stop_list
+
+	def init_group_map(self):
+		group_map = {}
+		dirs = [dir for dir in os.listdir(self.train_dir)]
+		for dir in dirs:
+			total = len([n for n in os.listdir(self.train_dir + "/" + dir)])
+			grand_total += total
+			fraction = Container()
+			fraction.numerator = 1
+			fraction.denominator = total
+			group_map[dir] = fraction
+		return group_map
+
 	def train_multivariate(self):
-		pass
+		words = VocabTrie(self.group_map)
+
+		
 
 	def train_multinomial(self):
 		pass
 
 	def train_smoothed(self):
 		pass
+
+class Container(object):
+	pass
+
+if __name__ == "__main__":
+	trainer = ModelTrainer(1, "C:/NaiveBayes/20news-bydate-train")
