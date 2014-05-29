1227eddd0ffd6a08fc02d401502439c900937fcf
willpalm <will5ryanpalmer@gmail.com>
2014-05-28 19:51:25 -0600

    fixed smoothing model and refactored

--- model_trainer.py
+++ model_trainer.py
@@ -206,17 +206,9 @@ class ModelTrainer(object):
         self.stop_words = self.fill_stop_list();
         self.train_dir = dir
         self.dirs = [dir for dir in os.listdir(self.train_dir)]
-        # self.group_map = self.init_group_map();
         self.group_map = {}
         self.total_files = 0
         self.train_models()
-
-        # if type == ModelTrainer.MULTIVARIATE:
-        #     self.train_multivariate()
-        # elif type == ModelTrainer.MULTINOMIAL:
-        #     self.train_multinomial()
-        # elif type == ModelTrainer.SMOOTHED:
-        #     self.train_smoothed()
 
     def fill_stop_list(self):
         stop_trie = VocabTrie("")
@@ -224,56 +216,6 @@ class ModelTrainer(object):
         for line in f:
             stop_trie.add_word(line.rstrip().lower(), 0)
         return stop_trie
-
-    def init_group_map(self):
-    	pass
-        # group_map = {}
-        # for my_dir in self.dirs:
-        #   total = len([n for n in os.listdir(self.train_dir + "/" + my_dir)])
-        #   fraction = Container()
-        #   fraction.numerator = 1
-        #   fraction.denominator = total
-        #   fraction.last_index = -1
-        #   group_map[my_dir] = fraction
-        # return group_map
-
-        # group_map = []
-        # for my_dir in self.dirs:
-        #     total = len([n for n in os.listdir(self.train_dir + "/" + my_dir)])
-        #     fraction = Container()
-        #     fraction.numerator = 1
-        #     fraction.denominator = total
-
-        #     fraction.name = my_dir
-        #     group_map.append(fraction)
-        # return group_map
-
-    # def train_multivariate(self):
-    # 	print "\nBegin Training"
-    # 	self.total_files = 0
-    #     for my_dir in self.dirs:
-    #         print my_dir
-    #         mytrie = VocabTrie(my_dir, ModelTrainer.MULTIVARIATE)
-    #         for index, f in enumerate(os.listdir(self.train_dir + "/" + my_dir)):
-    #             data_file = open(self.train_dir + "/" + my_dir + "/" + f, 'r')
-    #             for line in data_file:
-    #                 for word in line.split():
-    #                     total_words += 1
-    #                     tempword = ''.join(c for c in word if c.isalpha())
-    #                     tempword = tempword.lower()
-    #                     if len(tempword) > 0 and not self.stop_words.word_in_trie(tempword):
-    #                         word_stored = mytrie.word_in_trie(tempword)
-    #                         if not word_stored:
-    #                             for k, v in self.group_map.iteritems():
-    #                                 word_stored = v.word_in_trie(tempword)
-    #                                 if word_stored:
-    #                                     break
-    #                         if not word_stored:
-    #                             self.vocabulary.append(tempword)
-    #                         mytrie.add_word(tempword.lower(), index)
-    #             mytrie.total = index + 1
-    #             self.total_files += 1
-    #         self.group_map[mytrie.group] = mytrie
 
     def train_models(self):
         print "\nBegin Training"
@@ -293,27 +235,6 @@ class ModelTrainer(object):
                         	mytrie.add_word(tempword.lower(), index)
             self.group_map[mytrie.group] = mytrie
 
-    # def train_multinomial(self):
-    #     print "\nBegin Multinomial Training"
-    #     self.total_files = 0
-    #     for my_dir in self.dirs:
-    #         print my_dir
-    #         mytrie = VocabTrie(my_dir, ModelTrainer.MULTINOMIAL)
-    #         for index, f in enumerate(os.listdir(self.train_dir + "/" + my_dir)):
-    #             self.total_files += 1
-    #             data_file = open(self.train_dir + "/" + my_dir + "/" + f, 'r')
-    #             for line in data_file:
-    #                 for word in line.split():
-    #                     tempword = ''.join(c for c in word if c.isalpha())
-    #                     tempword = tempword.lower()
-    #                     if len(tempword) > 0 and not self.stop_words.word_in_trie(tempword):
-    #                         mytrie.add_word(tempword, index)
-    #                     mytrie.total += 1
-    #         self.group_map[mytrie.group] = mytrie
-
-    def train_smoothed(self):
-        pass
-
 class ModelTester(object):
     def __init__(self, type, trainer, dir):
         self.test_dir = dir
@@ -323,21 +244,7 @@ class ModelTester(object):
         self.stop_words = trainer.stop_words
         self.correct_map = {}
         self.confusion_matrix_row = {}
-
         self.initialize_confusion_matrix()
-
-        if type == ModelTrainer.MULTIVARIATE:
-            self.test("Multivariate")
-            f = open('Multivariate.csv', 'w')
-            f.write(self.filestr)
-        elif type == ModelTrainer.MULTINOMIAL:
-            self.test("Multinomial")
-            f = open('Multinomial.csv', 'w')
-            f.write(self.filestr)
-        elif type == ModelTrainer.SMOOTHED:
-            self.test("Smoothed")
-            f = open('Smoothed.csv', 'w')
-            f.write(self.filestr)
 
     def initialize_confusion_matrix(self):
     	self.filestr = ""
@@ -353,6 +260,24 @@ class ModelTester(object):
     		self.filestr += str(row[gp])
     	self.filestr += "\n"
 
+    def classify_single_document(self, model, my_dir, my_file):
+        words_in_doc = []
+        data_file = open(self.test_dir + "/" + my_dir + "/" + my_file)
+        for line in data_file:
+            for word in line.split():
+                tempword = ''.join(c for c in word if c.isalpha())
+                tempword = tempword.lower()
+                if len(tempword) > 0 and not self.stop_words.word_in_trie(tempword):
+                    words_in_doc.append(tempword)
+        if model == "Multivariate":
+            value = self.is_max_correct(my_dir, words_in_doc, ModelTrainer.MULTIVARIATE)
+        elif model == "Multinomial":    
+            value = self.is_max_correct(my_dir, words_in_doc, ModelTrainer.MULTINOMIAL)
+        else:
+            value = self.is_max_correct(my_dir, words_in_doc, ModelTrainer.SMOOTHED)
+        return value
+
+
     def test(self, model):
         print ("\nBegin " + str(model) + " Testing")
         for my_dir in self.dirs:
@@ -361,21 +286,8 @@ class ModelTester(object):
         		self.confusion_matrix_row[k] = 0
             total_correct = 0
             total = 0
-            for index, f in enumerate(os.listdir(self.test_dir + "/" + my_dir)):
-                words_in_doc = []
-                data_file = open(self.test_dir + "/" + my_dir + "/" + f, 'r')
-                for line in data_file:
-                    for word in line.split():
-                        tempword = ''.join(c for c in word if c.isalpha())
-                        tempword = tempword.lower()
-                        if len(tempword) > 0 and not self.stop_words.word_in_trie(tempword):
-                            words_in_doc.append(tempword)
-                if model == "Multivariate":
-                	value = self.is_max_correct(my_dir, words_in_doc)
-                elif model == "Multinomial":	
-                	value = self.is_max_correct_multinomial(my_dir, words_in_doc)
-                else:
-                	value = self.is_max_correct_smoothed(my_dir, words_in_doc)
+            for f in os.listdir(self.test_dir + "/" + my_dir):
+                value = self.classify_single_document(model, my_dir, f)
                 total_correct += value[0]
                 self.confusion_matrix_row[value[1]] += 1
                 total += 1
@@ -387,165 +299,29 @@ class ModelTester(object):
             print k
             print v
             total += (1.0 * v[0]) / v[1]
-        print (total * 1.0) / 20.0 
-
-
-    def test_multivariate(self):
-        print "\nBegin Multivariate Testing"
-        for my_dir in self.dirs:
-            print my_dir
-            total_correct = 0
-            total = 0
-            for index, f in enumerate(os.listdir(self.test_dir + "/" + my_dir)):
-                words_in_doc = []
-                data_file = open(self.test_dir + "/" + my_dir + "/" + f, 'r')
-                temp_trie = VocabTrie(my_dir)
-                for line in data_file:
-                    for word in line.split():
-                        tempword = ''.join(c for c in word if c.isalpha())
-                        tempword = tempword.lower()
-                        if len(tempword) > 0 and not self.stop_words.word_in_trie(tempword):
-                            temp_trie.add_word(tempword, index)
-                            words_in_doc.append(tempword)
-                total_correct += self.is_max_correct(my_dir, temp_trie, words_in_doc)
-                total += 1
-            self.correct_map[my_dir] = [total_correct, total]
-        print "\nMultivariate Results [correct, total]"
-        total = 0
-        for k, v in self.correct_map.iteritems():
-            print k
-            print v
-            total += (1.0 * v[0]) / v[1]
-        print (total * 1.0) / 20.0 
-
-    def test_multinomial(self):
-        print "\nBegin Multinomial Testing"
-        for my_dir in self.dirs:
-            print my_dir
-            total_correct = 0
-            total = 0
-            for index, f in enumerate(os.listdir(self.test_dir + "/" + my_dir)):
-                words_in_doc = []
-                data_file = open(self.test_dir + "/" + my_dir + "/" + f, 'r')
-                for line in data_file:
-                    for word in line.split():
-                        tempword = ''.join(c for c in word if c.isalpha())
-                        tempword = tempword.lower()
-                        if len(tempword) > 0 and not self.stop_words.word_in_trie(tempword):
-                            words_in_doc.append(tempword)
-                total_correct += self.is_max_correct_multinomial(my_dir, words_in_doc)
-                total += 1
-            self.correct_map[my_dir] = [total_correct, total]
-        print "\nMultinomial Results [correct, total]"
-        total = 0
-        for k, v in self.correct_map.iteritems():
-            print k
-            print v
-            total += (1.0 * v[0]) / v[1]
         print (total * 1.0) / 20.0
 
-    def test_smoothed():
-        print "\nBegin Multinomial Testing"
-        for my_dir in self.dirs:
-            print my_dir
-            total_correct = 0
-            total = 0
-            for index, f in enumerate(os.listdir(self.test_dir + "/" + my_dir)):
-                words_in_doc = []
-                data_file = open(self.test_dir + "/" + my_dir + "/" + f, 'r')
-                for line in data_file:
-                    for word in line.split():
-                        tempword = ''.join(c for c in word if c.isalpha())
-                        tempword = tempword.lower()
-                        if len(tempword) > 0 and not self.stop_words.word_in_trie(tempword):
-                            words_in_doc.append(tempword)
-                total_correct += self.is_max_correct_smoothed(my_dir, words_in_doc)
-                total += 1
-            self.correct_map[my_dir] = [total_correct, total]
-        print "\nMultinomial Results [correct, total]"
-        total = 0
-        for k, v in self.correct_map.iteritems():
-            print k
-            print v
-            total += (1.0 * v[0]) / v[1]
-        print (total * 1.0) / 20.0
-
-    def is_max_correct(self, group, words_in_doc):
-        highest = -sys.maxint
-        max_group = ""
-        # in_doc = []
-        # not_in_doc = []
-        # for word in self.vocabulary:
-        #   if temp_trie.word_in_trie(word):
-        #       in_doc.append(word)
-        #   else:
-        #       not_in_doc.append(word)
-        for k, v in self.group_map.iteritems():
-			num = 0
-			denom = 0
-			for word in words_in_doc:  
-				probability = (v.get_probability_type_given_group(word))
-				num += math.log(probability[0], 2)
-				denom += math.log(probability[1], 2)
-            # for word in in_doc:
-            #   probability *= v.get_probability_word_given_group(word)
-            # for word in not_in_doc:
-            #   probability *= (1 - v.get_probability_word_given_group(word))
-			num += math.log(v.doc_total, 2)
-			denom += math.log(self.total_files, 2)
-			probability = num - denom
-
-			if probability == 0:
-			    print "probability 0?"
-			if probability > highest:
-			    highest = probability
-			    max_group = k
-        if max_group == group:
-            return 1, max_group
-        else:
-            return 0, max_group
-
-    def is_max_correct_multinomial(self, group, words_in_doc):
+    def is_max_correct(self, group, words_in_doc, model_type):
         highest = -sys.maxint
         max_group = ""
         for k, v in self.group_map.iteritems():
             num = 0
             denom = 0
             for word in words_in_doc:
-                probability = (v.get_probability_token_given_group(word))
-                num += math.log(probability[0], 2)
+                if model_type == ModelTrainer.MULTIVARIATE:  
+                    probability = (v.get_probability_type_given_group(word))
+                elif model_type == ModelTrainer.MULTINOMIAL:
+                    probability = (v.get_probability_token_given_group(word))
+                else:
+                    probability = (v.get_probability_smoothed(word))
+                if not model_type == ModelTrainer.SMOOTHED or probability[0] != 0:
+                    num += math.log(probability[0], 2)
+                else:
+                    num -= 3
                 denom += math.log(probability[1], 2)
-        	# probability *= (1 + ((1.0 * v.doc_total) / self.total_files))
-        	num += math.log(v.doc_total, 2)
+            num += math.log(v.doc_total, 2)
             denom += math.log(self.total_files, 2)
             probability = num - denom
-            if probability == 0:
-                print "probability 0?"
-            if probability > highest:
-                highest = probability
-                max_group = k
-        if max_group == group:
-            return 1, max_group
-        else:
-            return 0, max_group
-
-    def is_max_correct_smoothed(self, group, words_in_doc):
-        highest = -sys.maxint
-        max_group = ""
-        for k, v in self.group_map.iteritems():
-            num = 0
-            denom = 0
-            for word in words_in_doc:
-                probability = (v.get_probability_token_given_group(word))
-                if probability[0] != 0:
-                	num += math.log(probability[0], 2)
-                	denom += math.log(probability[1], 2)
-        	# probability *= (1 + ((1.0 * v.doc_total) / self.total_files))
-        	num += math.log(v.doc_total, 2)
-            denom += math.log(self.total_files, 2)
-            probability = num - denom
-            if probability == 0:
-                print "probability 0?"
             if probability > highest:
                 highest = probability
                 max_group = k
@@ -556,9 +332,21 @@ class ModelTester(object):
 
 if __name__ == "__main__":
     trainer = ModelTrainer("20news/train")
-    tester = ModelTester(3, trainer, "20news/test")
-    tester = ModelTester(2, trainer, "20news/test")
-    tester = ModelTester(1, trainer, "20news/test")
+
+    tester = ModelTester(ModelTrainer.MULTIVARIATE, trainer, "20news/test")
+    tester.test("Multivariate")
+    f = open('Multivariate.csv', 'w')
+    f.write(tester.filestr)
+
+    tester = ModelTester(ModelTrainer.MULTINOMIAL, trainer, "20news/test")
+    tester.test("Multinomial")
+    f = open('Multinomial.csv', 'w')
+    f.write(tester.filestr)
+
+    tester = ModelTester(ModelTrainer.SMOOTHED, trainer, "20news/test")
+    tester.test("Smoothed")
+    f = open('Smoothed.csv', 'w')
+    f.write(tester.filestr)
     # print trainer.group_map["rec.sport.baseball"].get_probability_type_given_group("runs")
     
     
